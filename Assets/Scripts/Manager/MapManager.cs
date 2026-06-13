ba38cd41bf0c5b33a8df26e61d94f75a3abe7251
Xwilarg <xwilarg@protonmail.com>
2026-06-13 18:09:29 +0800

    Add map manager

---
+++ Assets/Scripts/Manager/MapManager.cs
@@ -0,0 +1,18 @@
+﻿using NsfwDelivery.Map;
+using UnityEngine;
+
+namespace NsfwDelivery.Manager
+{
+    public class MapManager : MonoBehaviour
+    {
+        public static MapManager Instance { private set; get; }
+
+        [SerializeField]
+        private Node _target;
+
+        public void CalculatePath()
+        {
+
+        }
+    }
+}
